fc713dd64d39e6ea288332708f253e5641cbb68e
Jessamine Magalona <jessaminemagalonaa@gmail.com>
2026-07-16 04:15:19 -0400

    Completed the second exercise for Day One

---
+++ 30DaysOfPython/helloworld.py
@@ -0,0 +1,19 @@
+# DAY ONE - 30DaysOfPython Challenge
+
+print(2 + 3)                            # Addition
+print(3 - 1)                            # Subtraction
+print(2 * 3)                            # Multiplication
+print(3 / 2)                            # Division
+print(3 ** 2)                           # Exponential
+print(3 % 2)                            # Modulus
+print(3 // 2)                           # Floor Division
+
+# Checking data types
+print(type(10))                         # Integer
+print(type(3.14))                       # Float
+print(type(1+3j))                       # Complex number
+print(type('Zachary Ezra Rawlins'))     # String
+print(type([1,2,3]))                    # List
+print(type({'name':'Asabeneh'}))        # Dictionary
+print(type({67, 3.14159, 21}))          # Set
+print(type((67, 3.14159, 21)))          # Tuple
